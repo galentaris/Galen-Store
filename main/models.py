ed68e67f919eb71ff46f992012008fe917c765d5
galentaris <galentaris01@gmail.com>
2023-09-13 11:10:16 +0700

    Revisi Testing

--- main/models.py
+++ main/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
+    price = models.IntegerField()
     amount = models.IntegerField()
     description = models.TextField()
--- main/tests.py
+++ main/tests.py
@@ -1,7 +1,5 @@
-from django.test import TestCase
-
-# Create your tests here.
 from django.test import TestCase, Client
+from main.models import Item
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -11,3 +9,33 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    #Penambahan testing diluar tutorial
+    def setUp(self):
+        Item.objects.create(
+            name = 'Kinder Joy', 
+            price = 10000, 
+            amount = 1, 
+            description = 'Makanan kesukaan Galasky',
+        )
+
+        Item.objects.create(
+            name = 'Green Tea', 
+            price = '20000', 
+            amount = 5, 
+            description = 'Minuman kesukaan owner',      
+        )
+
+    def test_item_1(self):
+        kinder_joy = Item.objects.get(name='Kinder Joy')
+        self.assertEqual(kinder_joy.name, 'Kinder Joy')
+        self.assertEqual(kinder_joy.price, 10000)
+        self.assertEqual(kinder_joy.amount, 1)
+        self.assertEqual(kinder_joy.description, 'Makanan kesukaan Galasky')
+
+    def test_item_2(self):
+        green_tea = Item.objects.get(name='Green Tea')
+        self.assertEqual(green_tea.name, 'Green Tea')
+        self.assertEqual(green_tea.price, 20000)
+        self.assertEqual(green_tea.amount, 5)
+        self.assertEqual(green_tea.description, 'Minuman kesukaan owner')
