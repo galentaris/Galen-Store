3ecf813a6ffdce2a4808663a874f6a0345756a92
galentaris <galentaris01@gmail.com>
2023-09-18 20:48:35 +0700

    Tes submit 1

--- main/tests.py
+++ main/tests.py
@@ -11,31 +11,31 @@ class mainTest(TestCase):
         self.assertTemplateUsed(response, 'main.html')
 
     #Penambahan testing diluar tutorial
-    def setUp(self):
-        Item.objects.create(
-            name = 'Kinder Joy', 
-            price = 10000, 
-            amount = 1, 
-            description = 'Makanan kesukaan Galasky',
-        )
+    # def setUp(self):
+    #     Item.objects.create(
+    #         name = 'Kinder Joy', 
+    #         price = 10000, 
+    #         amount = 1, 
+    #         description = 'Makanan kesukaan Galasky',
+    #     )
 
-        Item.objects.create(
-            name = 'Green Tea', 
-            price = '20000', 
-            amount = 5, 
-            description = 'Minuman kesukaan owner',      
-        )
+    #     Item.objects.create(
+    #         name = 'Green Tea', 
+    #         price = '20000', 
+    #         amount = 5, 
+    #         description = 'Minuman kesukaan owner',      
+    #     )
 
-    def test_item_1(self):
-        kinder_joy = Item.objects.get(name='Kinder Joy')
-        self.assertEqual(kinder_joy.name, 'Kinder Joy')
-        self.assertEqual(kinder_joy.price, 10000)
-        self.assertEqual(kinder_joy.amount, 1)
-        self.assertEqual(kinder_joy.description, 'Makanan kesukaan Galasky')
+    # def test_item_1(self):
+    #     kinder_joy = Item.objects.get(name='Kinder Joy')
+    #     self.assertEqual(kinder_joy.name, 'Kinder Joy')
+    #     self.assertEqual(kinder_joy.price, 10000)
+    #     self.assertEqual(kinder_joy.amount, 1)
+    #     self.assertEqual(kinder_joy.description, 'Makanan kesukaan Galasky')
 
-    def test_item_2(self):
-        green_tea = Item.objects.get(name='Green Tea')
-        self.assertEqual(green_tea.name, 'Green Tea')
-        self.assertEqual(green_tea.price, 20000)
-        self.assertEqual(green_tea.amount, 5)
-        self.assertEqual(green_tea.description, 'Minuman kesukaan owner')
+    # def test_item_2(self):
+    #     green_tea = Item.objects.get(name='Green Tea')
+    #     self.assertEqual(green_tea.name, 'Green Tea')
+    #     self.assertEqual(green_tea.price, 20000)
+    #     self.assertEqual(green_tea.amount, 5)
+    #     self.assertEqual(green_tea.description, 'Minuman kesukaan owner')
