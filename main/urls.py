6b45aed59d79e66d6e376931de9788c9db633b47
galentaris <galentaris01@gmail.com>
2023-11-22 10:42:42 +0700

    Revisi: Authentication

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import create_product_flutter, show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
 from main.views import register, login_user, logout_user, ubah_product, edit_product, get_product_json, add_product_ajax
 from main.views import delete_product_ajax
 
@@ -20,4 +20,5 @@ urlpatterns = [
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-ajax/', add_product_ajax, name='add_product_ajax'),
     path('delete-ajax/<int:id>', delete_product_ajax, name='delete_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
